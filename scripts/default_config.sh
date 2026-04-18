7913ad0c9f37a6b7e56e43ed3a0e81eaa9ef19ed
filin <alexmd@inbox.ru>
2026-04-18 20:54:12 +0700

    default_config.sh: версия 0.5, добавлены TARGET_DISTRO и TARGET_DEBIAN_*

--- scripts/default_config.sh
+++ scripts/default_config.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
 # This script provides common customization options for the ISO
-# 
-# Usage: Copy this file to config.sh and make changes there.  Keep this file (default_config.sh) as-is
-#   so that subsequent changes can be easily merged from upstream.  Keep all customiations in config.sh
+#
+# Usage: Copy this file to config.sh and make changes there. Keep this file (default_config.sh) as-is
+# so that subsequent changes can be easily merged from upstream. Keep all customisations in config.sh
 
-# The version of Ubuntu to generate.  Successfully tested LTS: bionic, focal, jammy, noble
+# Distro selector. Allowed: ubuntu, debian.
+export TARGET_DISTRO="${TARGET_DISTRO:-ubuntu}"
+
+# Ubuntu: tested >= noble (24.04). Older releases not supported.
 # See https://wiki.ubuntu.com/DevelopmentCodeNames for details
-export TARGET_UBUNTU_VERSION="focal"
-
-# The Ubuntu Mirror URL. It's better to change for faster download.
-# More mirrors see: https://launchpad.net/ubuntu/+archivemirrors
+export TARGET_UBUNTU_VERSION="noble"
 export TARGET_UBUNTU_MIRROR="https://archive.ubuntu.com/ubuntu/"
 
-# The packaged version of the Linux kernel to install on target image.
-# See https://wiki.ubuntu.com/Kernel/LTSEnablementStack for details
-export TARGET_KERNEL_PACKAGE="linux-generic"
+# Debian: tested >= trixie (13). forky (testing) and sid (unstable) accepted but experimental.
+export TARGET_DEBIAN_VERSION="trixie"
+export TARGET_DEBIAN_MIRROR="http://deb.debian.org/debian/"
+
+# Kernel package name. Override in config.sh if needed.
+# Ubuntu default: linux-generic. Debian default: linux-image-amd64.
+# If unset, the profile's KERNEL_PKG_DEFAULT is used.
+# export TARGET_KERNEL_PACKAGE="linux-generic"
 
 # The file (no extension) of the ISO containing the generated disk image,
 # the volume id, and the hostname of the live environment are set from this name.
@@ -27,7 +32,8 @@ export GRUB_LIVEBOOT_LABEL="Try Ubuntu FS without installing"
 # The text label shown in GRUB for starting installation
 export GRUB_INSTALL_LABEL="Install Ubuntu FS"
 
-# Packages to be removed from the target system after installation completes succesfully
+# Packages to be removed from the manifest shown to the installer (legacy ubiquity integration).
+# Not an apt purge - only affects casper/filesystem.manifest-desktop.
 export TARGET_PACKAGE_REMOVE="
     ubiquity \
     casper \
@@ -35,6 +41,10 @@ export TARGET_PACKAGE_REMOVE="
     laptop-detect \
     os-prober \
 "
+
+# Used to version the configuration. If breaking changes occur, manual
+# updates to this file from the default may be necessary.
+export CONFIG_FILE_VERSION="0.5"
 
 # Package customisation function.  Update this function to customize packages
 # present on the installed system.
@@ -65,7 +75,3 @@ function customize_image() {
         aisleriot \
         hitori
 }
-
-# Used to version the configuration.  If breaking changes occur, manual
-# updates to this file from the default may be necessary.
-export CONFIG_FILE_VERSION="0.4"
